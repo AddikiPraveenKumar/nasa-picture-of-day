3640ff12ead82d26e6b62d37b9e23e18d3d2d82a
AddikiPraveenKumar <addikipraveenkumar0726@gmail.com>
2025-10-01 19:54:03 +0100

    Initial Commit

---
+++ NASAAPODApp/NASAAPODAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAAPODAppApp.swift
+//  NASAAPODApp
+//
+//  Created by Praveen UK on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct NASAAPODAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
